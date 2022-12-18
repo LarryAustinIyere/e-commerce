872826534c1e4b3af20f7c907a19b9865a91b13f
LarryAustinIyere <austinlarry1506@gmail.com>
2022-12-19 00:55:18 +0100

    Update Navbar.jsx

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     return (
         <div className='navbar-container'>
             <p className='logo'>
-                <Link href='/'>Ecommerce test Store</Link>
+                <Link href='/'>Ecommerce Store</Link>
             </p>
 
             <button type='button' className='cart-icon' onClick={() => setShowCart(true)}>
